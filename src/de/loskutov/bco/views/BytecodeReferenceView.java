2fcf583af4e8ffabf966c89bea78c6d40c6cc5e2
ebruneton <ebruneton>
2005-04-21 08:32:04 +0000

    improved event listener registration

--- src/de/loskutov/bco/views/BytecodeReferenceView.java
+++ src/de/loskutov/bco/views/BytecodeReferenceView.java
@@ -16,8 +16,10 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IPartListener2;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartReference;
 import org.eclipse.ui.part.ViewPart;
 import org.osgi.framework.Bundle;
 
@@ -26,17 +28,58 @@ import org.objectweb.asm.util.AbstractVisitor;
 import de.loskutov.bco.BytecodeOutlinePlugin;
 
 
-public class BytecodeReferenceView extends ViewPart implements ISelectionListener {
+public class BytecodeReferenceView extends ViewPart implements IPartListener2, ISelectionListener {
 
     Browser browser;
     
     public void createPartControl(Composite parent) {
         browser = new Browser(parent, SWT.NONE);
-        getSite().getWorkbenchWindow().getSelectionService().addPostSelectionListener(this);
+        getSite().getWorkbenchWindow().getPartService().addPartListener(this);
     }
     
+    public void dispose() {
+        getSite().getWorkbenchWindow().getPartService().removePartListener(this);
+        super.dispose();
+    }
+
     public void setFocus() {
         /* nothing to do */
+    }
+
+    public void partActivated(IWorkbenchPartReference partRef) {
+        //
+    }
+
+    public void partBroughtToTop(IWorkbenchPartReference partRef) {
+        //
+    }
+
+    public void partClosed(IWorkbenchPartReference partRef) {
+        //
+    }
+
+    public void partDeactivated(IWorkbenchPartReference partRef) {
+        //
+    }
+
+    public void partOpened(IWorkbenchPartReference partRef) {
+        //
+    }
+
+    public void partHidden(IWorkbenchPartReference partRef) {
+        if (partRef.getId().equals(getSite().getId())) {
+            getSite().getWorkbenchWindow().getSelectionService().removePostSelectionListener(this);
+        }
+    }
+
+    public void partVisible(IWorkbenchPartReference partRef) {
+        if (partRef.getId().equals(getSite().getId())) {
+            getSite().getWorkbenchWindow().getSelectionService().addPostSelectionListener(this);
+        }
+    }
+
+    public void partInputChanged(IWorkbenchPartReference partRef) {
+        //
     }
 
     public void selectionChanged(IWorkbenchPart part, ISelection selection) {
